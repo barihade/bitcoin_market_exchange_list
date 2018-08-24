16992716ab4ce9c88ec4b4a6c930a708155f5e41
Mocha. N. Fadillah <mochamadfadilah88@gmail.com>
2018-08-24 13:52:04 +0700

    remove jquery idex

--- idex/index.php
+++ idex/index.php
@@ -89,7 +89,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
     <script src="assets/js/bootstrap.bundle .js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
   </body>
